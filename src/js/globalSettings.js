de283bdb48def76ab1d11f0bd522082e4d6d349e
Rudiko777 <chakryan_rudik@mail.ru>
2023-11-29 09:09:26 +0300

    Updates

--- src/js/globalSettings.js
+++
@@ -1,7 +0,0 @@
-export const linkRussians = {
-    main: ['', "Главная"],
-    credit: ["credit","Кредит"],
-    authorization: ["Auth", "Авторизация"],
-    disposal: ['disposal', "Утилизация"],
-    taxi: ['taxi', "Такси в кредит"]
-}
